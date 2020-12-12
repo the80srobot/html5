557bd538d3c7022808c034e27725db457f3a43a2
Adam Sindelar <adam.sindelar@gmail.com>
2020-12-12 14:50:55 +0100

    Allow raw to use bindings

--- html/raw.go
+++ html/raw.go
@@ -5,10 +5,15 @@ type RawNode struct {
 }
 
 func (r *RawNode) compile(tc *templateCompiler, _ int, _ *CompileOptions) error {
-	s, err := r.HTML.Convert(FullyTrusted)
-	if err != nil {
+	if r.HTML.Constant() {
+		s, err := r.HTML.Convert(FullyTrusted)
+		if err != nil {
+			return err
+		}
+		_, err = tc.WriteString(s)
 		return err
 	}
-	_, err = tc.WriteString(s)
-	return err
+
+	tc.appendStringBinding(r.HTML.binding, FullyTrusted)
+	return nil
 }
