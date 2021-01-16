0154fa3c8db5e3178d57fe0a7cee3ae6d93ff379
Adam Sindelar <adam.sindelar@gmail.com>
2021-01-16 14:56:52 +0100

    Provide element prototypes
    This avoids having to specify things like SelfClosing all the time.

--- element.go
+++ element.go
@@ -16,11 +16,14 @@ type ElementNode struct {
 }
 
 func Element(name string, contents ...Content) *ElementNode {
-	e := &ElementNode{Name: name}
-	for _, c := range contents {
-		c.Apply(e)
+	e, ok := elementPrototypes[name]
+	if !ok {
+		e = ElementNode{Name: name}
 	}
-	return e
+	for _, c := range contents {
+		c.Apply(&e)
+	}
+	return &e
 }
 
 func (e *ElementNode) Apply(n Node) error {
@@ -109,4 +112,71 @@ func (e *ElementNode) compile(tc *templateCompiler, depth int, opts *CompileOpti
 	}
 
 	return nil
+}
+
+var elementPrototypes = map[string]ElementNode{
+	// HTML5 void elements: self-closing by default.
+	"area":    {Name: "area", IndentStyle: Block, SelfClosing: true},
+	"base":    {Name: "base", IndentStyle: Inline, SelfClosing: true},
+	"br":      {Name: "br", IndentStyle: Inline, SelfClosing: true},
+	"command": {Name: "command", IndentStyle: Inline, SelfClosing: true},
+	"embed":   {Name: "embed", IndentStyle: Block, SelfClosing: true},
+	"hr":      {Name: "hr", IndentStyle: Block, SelfClosing: true},
+	"img":     {Name: "img", IndentStyle: Inline, SelfClosing: true},
+	"input":   {Name: "input", IndentStyle: Block, SelfClosing: true},
+	"keygen":  {Name: "keygen", IndentStyle: Block, SelfClosing: true},
+	"link":    {Name: "link", IndentStyle: Block, SelfClosing: true},
+	"meta":    {Name: "meta", IndentStyle: Block, SelfClosing: true},
+	"param":   {Name: "param", IndentStyle: Block, SelfClosing: true},
+	"source":  {Name: "source", IndentStyle: Block, SelfClosing: true},
+	"track":   {Name: "track", IndentStyle: Block, SelfClosing: true},
+	"wbr":     {Name: "wbr", IndentStyle: Inline, SelfClosing: true},
+
+	// Block indent by default.
+	"article":    {Name: "article", IndentStyle: Block, SelfClosing: false},
+	"address":    {Name: "address", IndentStyle: Block, SelfClosing: false},
+	"aside":      {Name: "aside", IndentStyle: Block, SelfClosing: false},
+	"body":       {Name: "body", IndentStyle: Block, SelfClosing: false},
+	"blockquote": {Name: "blockquote", IndentStyle: Block, SelfClosing: false},
+	"button":     {Name: "button", IndentStyle: Block, SelfClosing: false},
+	"canvas":     {Name: "canvas", IndentStyle: Block, SelfClosing: false},
+	"caption":    {Name: "caption", IndentStyle: Block, SelfClosing: false},
+	"code":       {Name: "code", IndentStyle: Block, SelfClosing: false},
+	"colgroup":   {Name: "colgroup", IndentStyle: Block, SelfClosing: false},
+	"datalist":   {Name: "datalist", IndentStyle: Block, SelfClosing: false},
+	"dl":         {Name: "dl", IndentStyle: Block, SelfClosing: false},
+	"fieldset":   {Name: "fieldset", IndentStyle: Block, SelfClosing: false},
+	"figcaption": {Name: "figcaption", IndentStyle: Block, SelfClosing: false},
+	"figure":     {Name: "figure", IndentStyle: Block, SelfClosing: false},
+	"footer":     {Name: "footer", IndentStyle: Block, SelfClosing: false},
+	"form":       {Name: "form", IndentStyle: Block, SelfClosing: false},
+	"h1":         {Name: "h1", IndentStyle: Block, SelfClosing: false},
+	"h2":         {Name: "h2", IndentStyle: Block, SelfClosing: false},
+	"h3":         {Name: "h3", IndentStyle: Block, SelfClosing: false},
+	"h4":         {Name: "h4", IndentStyle: Block, SelfClosing: false},
+	"h5":         {Name: "h5", IndentStyle: Block, SelfClosing: false},
+	"h6":         {Name: "h6", IndentStyle: Block, SelfClosing: false},
+	"head":       {Name: "head", IndentStyle: Block, SelfClosing: false},
+	"header":     {Name: "header", IndentStyle: Block, SelfClosing: false},
+	"html":       {Name: "html", IndentStyle: Block, SelfClosing: false},
+	"hgroup":     {Name: "hgroup", IndentStyle: Block, SelfClosing: false},
+	"iframe":     {Name: "iframe", IndentStyle: Block, SelfClosing: false},
+	"legend":     {Name: "legend", IndentStyle: Block, SelfClosing: false},
+	"nav":        {Name: "nav", IndentStyle: Block, SelfClosing: false},
+	"p":          {Name: "p", IndentStyle: Block, SelfClosing: false},
+	"ol":         {Name: "ol", IndentStyle: Block, SelfClosing: false},
+	"optgroup":   {Name: "optgroup", IndentStyle: Block, SelfClosing: false},
+	"samp":       {Name: "samp", IndentStyle: Block, SelfClosing: false},
+	"script":     {Name: "script", IndentStyle: Block, SelfClosing: false},
+	"section":    {Name: "section", IndentStyle: Block, SelfClosing: false},
+	"style":      {Name: "style", IndentStyle: Block, SelfClosing: false},
+	"table":      {Name: "table", IndentStyle: Block, SelfClosing: false},
+	"tbody":      {Name: "tbody", IndentStyle: Block, SelfClosing: false},
+	"textarea":   {Name: "textarea", IndentStyle: Block, SelfClosing: false},
+	"tfoot":      {Name: "tfoot", IndentStyle: Block, SelfClosing: false},
+	"thead":      {Name: "thead", IndentStyle: Block, SelfClosing: false},
+	"title":      {Name: "title", IndentStyle: Block, SelfClosing: false},
+	"ul":         {Name: "ul", IndentStyle: Block, SelfClosing: false},
+
+	// Everything else defaults to paired tags (non-void), inline.
 }
